e2c66a041e95b5c9443fd5099c1dddaf0632f6d7
Karim Nadim <karim_ossama94@hotmail.com>
2025-08-27 16:20:45 -0400

    updated requirements and uploaded all files

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
+    print(f"tpye of data before reshaping: {type(data)}")
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
@@ -34,6 +35,6 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
    
      
